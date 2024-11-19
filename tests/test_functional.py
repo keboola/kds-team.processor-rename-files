2e2078f9179fb8e770d5f90a7a25308b6d9620c7
ZdenekSrotyr <zdenek.srotyr@keboola.com>
2024-11-19 12:36:55 +0100

    fix tests

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -3,10 +3,11 @@ from pathlib import Path
 import unittest
 
 from datadirtest import DataDirTester
+from freezegun import freeze_time
 
 
 class TestComponent(unittest.TestCase):
-
+    @freeze_time("2010-10-10")
     def test_functional(self):
         functional_tests = DataDirTester(Path(__file__).parent.joinpath('functional'),
                                          Path(__file__).parent.parent.joinpath('src/component.py'))
